f8648d17262cf50539605084401143a3a410402f
Falco13 <keanu0608@gmail.com>
2023-03-04 22:58:34 +0200

    fix model fields for admin site

--- contact_email_app/apps.py
+++ contact_email_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ContactEmailAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact_email_app'
+    verbose_name='Section Contacts'
--- contact_email_app/models.py
+++ contact_email_app/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=15)
     email = models.EmailField()
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_done = models.BooleanField(default=False)
+    text = models.TextField(verbose_name='message')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='update date')
+    is_done = models.BooleanField(default=False, verbose_name='done')
 
     def __str__(self):
         return f'Name: {self.name}, Done: {self.is_done}'
