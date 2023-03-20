f763939ef65036e3cfbeb2b3ee1a2b35c5cd6371
Falco13 <keanu0608@gmail.com>
2023-03-21 01:05:51 +0200

    short_text for admin site

--- contact_email_app/admin.py
+++ contact_email_app/admin.py
@@ -4,5 +4,11 @@ from contact_email_app.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email', 'text', 'created_at', 'updated_at', 'is_done']
+    list_display = ['id', 'name', 'email', 'short_text', 'created_at', 'updated_at', 'is_done']
     readonly_fields = ['id', 'name', 'email', 'text', 'created_at', 'updated_at']
+
+    @staticmethod
+    def short_text(obj: Contact):
+        if len(obj.text) < 150:
+            return obj.text
+        return obj.text[:150] + '...'
